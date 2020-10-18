bf0ca4693181bb29ed65ad4d07cca59fa002f69d
Christopher Leggett <chris@leggett.dev>
2020-10-18 17:43:08 -0400

    Adds user authentication

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/leggettc18/hackernews-clone-api
 go 1.14
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/graph-gophers/graphql-go v0.0.0-20201003130358-c5bdf3b1108e // indirect
 	github.com/rs/cors v1.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
 )
--- main.go
+++ main.go
@@ -7,7 +7,9 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/rs/cors"
+	"golang.org/x/crypto/bcrypt"
 
 	graphql "github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
@@ -24,12 +26,33 @@ var (
 	}
 )
 
+var users []User = []User{}
+
+type User struct {
+	ID       graphql.ID
+	Name     string
+	Email    string
+	Password string
+	Links    []Link
+}
+
+type AuthPayload struct {
+	Token *string
+	User  *User
+}
+
 type RootResolver struct{}
 
 type Link struct {
 	ID          graphql.ID
 	Description string
 	URL         string
+	PostedBy    Poster
+}
+
+type Poster struct {
+	ID   graphql.ID
+	Name string
 }
 
 func (r *RootResolver) Info() (string, error) {
@@ -58,15 +81,110 @@ func (r *RootResolver) Link(args struct {
 func (r *RootResolver) Post(args struct {
 	Description string
 	URL         string
+	PostedBy    string
 }) (Link, error) {
+	userName, errID := getNameFromID(args.PostedBy)
+	if errID != nil {
+		return Link{}, errID
+	}
 	newLink := Link{
 		ID:          graphql.ID(fmt.Sprint(len(links))),
 		Description: args.Description,
 		URL:         args.URL,
+		PostedBy: Poster{
+			ID:   graphql.ID(fmt.Sprint(args.PostedBy)),
+			Name: userName,
+		},
 	}
 
 	links = append(links, newLink)
 	return newLink, nil
+}
+
+func getNameFromID(id string) (string, error) {
+	for _, user := range users {
+		if user.ID == graphql.ID(id) {
+			return user.Name, nil
+		}
+	}
+	return "", errors.New("User ID not found")
+}
+
+func (r *RootResolver) Signup(args struct {
+	Email    string
+	Password string
+	Name     string
+}) (*AuthPayload, error) {
+	passwordHash, errHash := bcrypt.GenerateFromPassword(
+		[]byte(args.Password),
+		bcrypt.DefaultCost,
+	)
+	if errHash != nil {
+		return nil, errHash
+	}
+
+	newUser := User{
+		ID:       graphql.ID(fmt.Sprint(len(users))),
+		Email:    args.Email,
+		Password: string(passwordHash),
+		Name:     args.Name,
+	}
+
+	users = append(users, newUser)
+
+	token, errToken := GenerateToken(&newUser)
+
+	if errToken != nil {
+		return nil, errToken
+	}
+
+	return &AuthPayload{
+		Token: &token,
+		User:  &newUser,
+	}, nil
+}
+
+func GenerateToken(user *User) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"ID": user.ID,
+	})
+	tokenString, errToken := token.SignedString([]byte("verysecret"))
+	if errToken != nil {
+		return "", errToken
+	}
+	return tokenString, nil
+}
+
+func (r *RootResolver) Login(args struct {
+	Email    string
+	Password string
+}) (*AuthPayload, error) {
+	user, errUser := getUser(args.Email, args.Password)
+	if errUser != nil {
+		return nil, errUser
+	}
+
+	token, errToken := GenerateToken(&user)
+	if errToken != nil {
+		return nil, errToken
+	}
+	return &AuthPayload{
+		Token: &token,
+		User:  &user,
+	}, nil
+}
+
+func getUser(email, password string) (User, error) {
+	for _, u := range users {
+		if u.Email == email {
+			errCompare := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+			if errCompare != nil {
+				return User{}, errCompare
+			}
+			return u, nil
+		}
+	}
+	return User{}, errors.New("No user with email " + email)
 }
 
 // Reads and parses the schema from file.
